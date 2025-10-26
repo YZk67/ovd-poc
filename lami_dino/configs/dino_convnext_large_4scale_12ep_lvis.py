c8c80ed1daee0045723f16290925f91e026a69d8
Zhengjian Kang <zhengjiankang@Zhengjians-MacBook-Pro.local>
2025-10-26 18:38:28 -0400

    fix: reduce lr to 1.5x

--- lami_dino/configs/dino_convnext_large_4scale_12ep_lvis.py
+++ lami_dino/configs/dino_convnext_large_4scale_12ep_lvis.py
@@ -81,10 +81,10 @@ model.eval_query_path = "dataset/metadata/lvis_claude_prompts_convnextl.npy"
 # model.select_box_nums_for_evaluation = 300
 
 # modify optimizer config
-# 假设你单卡用 1e-4 (1GPU); 1e-4 (8 GPUs) - 使用原来成功的学习率
+# 假设你单卡用 1e-4 (1GPU); 1e-4 使用原来成功的学习率
 base_lr = 1e-4
-world_size = 4  # GPU 数
-optimizer.lr = base_lr * world_size  # 4e-4
+world_size = 1.5  # GPU 数
+optimizer.lr = base_lr * world_size
 optimizer.betas = (0.9, 0.999)
 optimizer.weight_decay = 1e-4
 optimizer.params.lr_factor_func = lambda module_name: 0.1 if "backbone" in module_name else 1
