2d3751fe10aaa61fea11dd2ff529092185d67abe
YZk67 <kirazhang86@gmail.com>
2025-12-01 16:37:27 -0800

    update

--- detectron2/detectron2/data/datasets/builtin.py
+++ detectron2/detectron2/data/datasets/builtin.py
@@ -30,26 +30,28 @@ from .lvis import get_lvis_instances_meta, register_lvis_instances
 from .pascal_voc import register_pascal_voc
 import json
 
+# ==================================================================
+# 直接修改原来的 COCO65 变量，放入 80 类全集
+# ==================================================================
 COCO65 = [
-    "person","bicycle","car","motorcycle","airplane","bus","train","truck","boat","bench",
-    "bird","cat","dog","horse","sheep","cow",
-    "elephant","bear","zebra","giraffe",
-    "backpack","umbrella","handbag","tie","suitcase",
-    "frisbee","skis","snowboard","kite","skateboard",
-    "surfboard","bottle","cup","fork","knife","spoon","bowl",
-    "banana","apple","sandwich","orange","broccoli","carrot","pizza","donut","cake",
-    "chair","couch","bed","toilet","tv","laptop","mouse","remote","keyboard",
-    "microwave","oven","toaster","sink","refrigerator","book","clock","vase",
-    "scissors","toothbrush"
+    "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat",
+    "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+    "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack",
+    "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball",
+    "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket",
+    "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+    "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
+    "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop",
+    "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink",
+    "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier",
+    "toothbrush"
 ]
 
+# 同时也必须修改对应的 ID 列表 (这是标准 COCO 的 1-90 ID，对应上面的 80 类)
 COCO65_DATASET_IDS = [
-    1,2,3,4,5,6,7,8,9,      # person..boat
-    15,16,17,18,19,20,21,22,23,24,25,
-    27,28,31,32,33,34,35,36,38,41,42,
-    44,47,48,49,50,51,52,53,54,55,56,57,
-    59,60,61,62,63,65,70,72,73,74,75,76,
-    78,79,80,81,82,84,85,86,87,90
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34,
+    35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
+    64, 65, 67, 70, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 84, 85, 86, 87, 88, 89, 90
 ]
 # 顺序必须与 COCO65 名称列表一一对应（你当前的 COCO65 顺序是对的）
 
