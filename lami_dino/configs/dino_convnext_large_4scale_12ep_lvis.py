c0c40573de960d9dd99fff9b2c543af3ea782836
YZk67 <kirazhang86@gmail.com>
2025-12-08 20:44:08 -0800

    fix

--- lami_dino/configs/dino_convnext_large_4scale_12ep_lvis.py
+++ lami_dino/configs/dino_convnext_large_4scale_12ep_lvis.py
@@ -52,12 +52,12 @@ model.num_classes = 80
 model.query_path = "dataset/metadata/coco_80_mean_aligned_fixed1.npy"
 model.eval_query_path = "dataset/metadata/coco_80_mean_aligned_fixed1.npy"
 
-#model.use_fed_loss = True
-#model.cluster_fed_loss = True
-#model.cluster_label_path = 'dataset/cluster/lvis_cluster_128.npy'
-#model.cat_freq_path = "dataset/lvis/lvis_v1_train_norare_cat_info.json"
-#model.fed_loss_num_cat=100
-#model.select_box_nums_for_evaluation = 300
+model.use_fed_loss = True
+model.cluster_fed_loss = True
+model.cluster_label_path = 'dataset/cluster/coco_cluster_20.npy'
+model.cat_freq_path = "dataset/coco/ovd_ins_train2017_b_cat_info.json"
+model.fed_loss_num_cat=100
+model.select_box_nums_for_evaluation = 300
 
 # modify optimizer config
 optimizer.lr = 1e-4
