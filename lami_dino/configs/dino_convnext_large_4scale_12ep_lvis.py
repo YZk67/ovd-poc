cb54f76198df5129fe8ae460e34d5759896cfe44
YZk67 <kirazhang86@gmail.com>
2026-01-24 01:41:52 -0800

    fixovdcoco65

--- lami_dino/configs/dino_convnext_large_4scale_12ep_lvis.py
+++ lami_dino/configs/dino_convnext_large_4scale_12ep_lvis.py
@@ -84,7 +84,7 @@ model.select_box_nums_for_evaluation = 300
 # modify optimizer config
 # 假设你单卡用 1e-4 (1GPU); 1e-4 使用原来成功的学习率
 base_lr = 1e-4
-world_size = 2  # GPU 数
+world_size = 1.5  # GPU 数
 optimizer.lr = base_lr * world_size
 optimizer.betas = (0.9, 0.999)
 optimizer.weight_decay = 1e-4
