b1f721ac0079401e0ab6db07d23c2f315ee4918e
YZk67 <kirazhang86@gmail.com>
2026-02-20 13:42:22 -0800

    fixBug3

--- lami_dino/configs/dino_convnext_large_4scale_12ep_lvis.py
+++ lami_dino/configs/dino_convnext_large_4scale_12ep_lvis.py
@@ -105,7 +105,7 @@ model.eval_query_path = "dataset/metadata/ovdcoco_prompts_list8_v2.npy"
 
 model.use_fed_loss = True
 model.cluster_fed_loss = True
-model.cluster_label_path = 'dataset/cluster/ovdcoco_cluster_16.npy'
+model.cluster_label_path = 'dataset/cluster/ovdcoco_cluster_161.npy'
 model.cat_freq_path = "dataset/coco/ovd_ins_train2017_all_cat_info.json"
 model.fed_loss_num_cat = 20
 model.select_box_nums_for_evaluation = 2000  # balance recall vs noise
